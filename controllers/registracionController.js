af7ffb3d1796f1865f63125825f4db1e6f245104
francesca <franchuporro@hotmail.com>
2020-05-21 19:51:02 -0300

    encriptado de contraseña, punto 3 terminado

--- controllers/registracionController.js
+++ controllers/registracionController.js
@@ -1,4 +1,6 @@
 let db = require ('../database/models/index');
+const bcrypt = require('bcryptjs');
+
 const funciones={
     registracion : function(req,res){
         res.render('registracion');
@@ -6,11 +8,16 @@ const funciones={
     crear: function(req,res){
 
     },
+    
     guardar: function(req,res) {
+        let passEncriptada = bcrypt.hashSync(req.body.password, 10) ;
+       // let check = bcrypt.compareSync ('req.body.password', passEncriptada);
+       // console.log(check); // true
+    
         db.Usuario.create ({
             nombreCompleto: req.body.nombre ,
             email:req.body.email ,
-            password: req.body.password  ,
+            password: passEncriptada  ,
             fechaNacimiento: req.body.Birthday  ,
             createAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
          //   movieid: req.query.idmovie
@@ -30,4 +37,7 @@ const funciones={
     }
     
     }
+  
+
     module.exports= funciones;
+   
