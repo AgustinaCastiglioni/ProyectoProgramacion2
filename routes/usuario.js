a33ad48db93e0cb71e16b7df8664806c05eea1d6
francesca <franchuporro@hotmail.com>
2020-06-06 12:03:21 -0300

    punto 7 faltan las vistas

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var controller= require('../controllers/usuarioController')
-router.get('/login', controller.logUser);
-router.post('/login', controller.confirmUser);
-router.get ('/login/:id', controller.getReviews);
+router.get('/login', controller.logUser); // form de logueo
+router.post('/login', controller.confirmUser); //procesa loguea y redirecciona a MIS RESEÑAS
+router.get ('/usuario/resenas/:id', controller.getReviews); //LISTADO DE MIS RESENAS
+router.get('/resenas/editReview/:id', controller.showEdit);// form para editar
+router.post('/resenas/editReview/:id', controller.confirmEdit); // procesa la edicion de la resena
+router.get('/resenas/deleteReview/:id', controller.deleteReview);
+router.post('/resenas/deleteReview/:id', controller.confirmDelete);
 router.get('/:id', controller.datosUsuario);
 
 
